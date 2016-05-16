21169fcc9b831c0536c0b2328849ad31a33f2daa
Vis Virial <visvirial@gmail.com>
2016-05-17 00:43:26 +0900

    Handles URLs in bitFlyer chat.

--- src/BitFlyerChat.js
+++ src/BitFlyerChat.js
@@ -7,6 +7,9 @@ var BitFlyerChat = function(config) {
 	this.client = new signalR.client('wss://lightning.bitflyer.jp/signalr', ['BFEXHub']);
 	var this_ = this;
 	this.client.on('BFEXHub', 'addNewMessageToPage', function(name, message) {
+		// Replace URLs.
+		message = message.replace(/<a href="(.+?)" target="_blank">(.+)<\/a>/gm, '<$1|$2>');
+		// Replace glossary words.
 		message = message.replace(/<a href="(.+?)" target="glossary" title="((.|\n|\r\n)+?)">(.+)<\/a>/gm, '<$1|$4>');
 		this_.slack.send('BitFlyer', name, message);
 	});
